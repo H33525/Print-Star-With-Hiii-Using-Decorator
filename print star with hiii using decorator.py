a86999908531e303d449ccc4e4d393fe2d4a8cfc
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-09-01 19:52:47 +0530

    Add files via upload

---
+++ print star with hiii using decorator.py
@@ -0,0 +1,13 @@
+def my_decorator(func):
+
+def wrap_func(x):
+   print('**********')
+func(x)
+   print('**********')
+return wrap_func
+
+@my_decorator
+   def hello(greeting):
+   print(greeting)
+
+hello('hiiiiiii')
